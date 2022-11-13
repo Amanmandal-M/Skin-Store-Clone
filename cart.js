9346094650e6f06778f93c2d2b31d002dd15c21f
Aman Kumar <amanmandal644@gmail.com>
2022-11-13 08:32:38 +0530

    one step ahead

---
+++ cart.js
@@ -0,0 +1,29 @@
+let cart_2 = JSON.parse(localStorage.getItem(("cart_items")));
+displayData(cart_2);
+
+
+    function displayData(out) {
+        document.querySelector("#container").innerHTML = "";
+        let bag="";
+      out.forEach(elem => {
+        var div = document.createElement("div");
+    
+        var image = document.createElement("img");
+        image.setAttribute("src", elem.image);
+    
+        var Title = document.createElement("h3");
+        Title.innerText = elem.title;
+    
+        var Price = document.createElement("p");
+        Price.innerText = "Rs. "+ elem.price;     
+
+        div.append(image, Title, Price,but);
+    
+        document.querySelector("#container").append(div);
+      });
+    }
+
+let total = cart_2.reduce((acc,el)=>{   
+   return acc + Number(el.price);       
+},0)
+document.querySelector("#class").innerText="Total Price :"+ " Rs. "+total;
--- product.js
+++ product.js
@@ -1,43 +1,117 @@
 var url = "https://636f9027f2ed5cb047e01947.mockapi.io/Product_Page_2";
 
 var out = [];
+var cart_1 =JSON.parse(localStorage.getItem(JSON.stringify("cart_items"))) || [];
 
 async function getData() {
-    try{
-        var res = await fetch(url);
-        var data = await res.json();
-        out=[...data];
-        displayData(out);
-    }
-    catch(e){
-        console.log(e)
-    }
+  try {
+    var res = await fetch(url);
+    var data = await res.json();
+    out = [...data];
+    displayData(out);
+  } catch (e) {
+    console.log(e);
+  }
 }
 getData();
 
 
-function displayData(out){
+function displayData(out) {
+    document.querySelector("#container").innerHTML = "";
     
-        for(var i=0; i<33; i++){
-            var div = document.createElement("div");
+  out.forEach(elem => {
 
-            var image = document.createElement("img");
-            image.setAttribute("src", out[i].image);
+    var div = document.createElement("div");
 
-            var Title = document.createElement("h3");
-            Title.innerText = out[i].title;
+    var image = document.createElement("img");
+    image.setAttribute("src", elem.image);
 
-            var btn_1 = document.createElement("button");
-            btn_1.innerText = "25% off with code SINGLES";
+    var Title = document.createElement("h3");
+    Title.innerText = elem.title;
 
-            var Price = document.createElement("p");
-            Price.innerText = out[i].price;
+    var cat = document.createElement("p");
+    cat.innerText = elem.category;
 
-            var btn_2 = document.createElement("button");
-            btn_2.innerText = "Quick Buy";
+    var Price = document.createElement("p");
+    Price.innerText = "Rs. "+elem.price;
 
-            div.append(image,Title,btn_1,Price,btn_2);
+    var btn_2 = document.createElement("button");
+    btn_2.innerText = "Quick Buy";
 
-            document.querySelector("#container").appendChild(div);   
-        }
+    btn_2.addEventListener("click",function(){
+        cart_1.push(elem);
+        localStorage.setItem("cart_items",JSON.stringify(cart_1));
+    });
+
+    div.append(image, Title, cat , Price, btn_2);
+
+    document.querySelector("#container").appendChild(div);
+  });
+    
+  }
+
+
+//Sorting
+
+function Sorting() {
+  let selects = document.querySelector("#selected").value;
+
+        if (selects == "asc") {
+            out.sort((a, b) => {
+              return a.price-b.price;
+            });
+          } else {
+            if (selects == "dsc") {
+              out.sort((a, b) => {
+                return b.price - a.price;
+              });
+            }
+
+          }
+  displayData(out);
 }
+
+
+
+document.querySelector("#selected").addEventListener("change", myChange);
+
+function myChange (e) {
+  Sorting();
+  filter(out);
+}
+
+document.querySelector("#val").addEventListener("change", myFunc);
+
+function myFunc(e){
+    filter(out);
+}
+
+//filter
+
+function filter(out){
+    let sele = document.querySelector("#val").value;
+
+    let filterData = out.filter((elem) =>{
+        if (elem.category == sele) {
+          return elem.category;
+        }else{
+            if(sele ==""){
+              return elem.category;
+            }
+        }
+      });
+    displayData(filterData);
+}
+
+//Search
+
+document.querySelector("#search_00").addEventListener("input", function(){
+    let searched = document.querySelector("#search_00").value;
+    console.log(searched);
+
+    let newData_1 = out.filter(function(elem){
+         return elem.title.toLowerCase().includes(searched.toLowerCase());
+    });
+    displayData(newData_1)
+});
+
